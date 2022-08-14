81d1b12ea64c52ba5223e824c317572eae823ebc
jjl <jlyden1@alumni.nd.edu>
2022-08-13 20:38:35 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app: Express = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>hello from typescript world</h1>');
+});
+
+app.listen(PORT, () => console.log(`running on ${PORT} ⚡`));
